8cc3e9a1fa6cddb3ecac9c103a21971cc2f26d2a
Matt Tkocz <mtkocz@Mac.attlocal.net>
2026-03-04 12:47:22 -0800

    Your message here

--- js/tank/Tank.js
+++ js/tank/Tank.js
@@ -1662,7 +1662,7 @@ Tank.updateLeanState = function (lean, speed, heading, deltaTime, isDead) {
 
   const turnRate = headingDelta / deltaTime;
   const speedFactor = Math.min(Math.abs(speed) / 0.0004, 1.0);
-  let steerTarget = -(turnRate * speedFactor * cfg.STEER_LEAN_GAIN);
+  let steerTarget = turnRate * speedFactor * cfg.STEER_LEAN_GAIN;
   steerTarget = Math.max(-cfg.MAX_STEER_LEAN, Math.min(cfg.MAX_STEER_LEAN, steerTarget));
 
   const steerError = steerTarget - lean.steerAngle;
